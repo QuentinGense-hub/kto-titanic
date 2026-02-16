30dc9e89cf70068400dab6cac41be43d45bec803
QuentinGense-hub <quentin.gense@gmail.com>
2026-02-16 13:12:54 +0000

    feat: exercice d'évaluation clean code

--- exercices/mon_premier_script.py
+++ exercices/mon_premier_script.py
@@ -14,14 +14,11 @@ class FirstName:
         return len(self.value) > MINIMUM_LENGTH_EXCLUSIVE
 
 
-def count_first_names_longer_than_seven_letters(first_names: List[FirstName]) -> int: #Typage complet
-    long_first_names_count: int = 0
-
-    for first_name in first_names:
-        if first_name.has_more_than_seven_letters():
-            long_first_names_count += 1
-
-    return long_first_names_count
+def count_first_names_longer_than_seven_letters(first_names: List[FirstName]) -> int: #Typage
+    return sum(
+        first_name.has_more_than_seven_letters()
+        for first_name in first_names
+    )
 
 
 class TestCountFirstNames(unittest.TestCase): #Test lisible
